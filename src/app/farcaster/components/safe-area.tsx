2a9c01d6fc313262aecea03d5d188858c5dd6003
Pablo <pahhhblo@proton.me>
2025-06-11 14:53:39 -0400

    test

--- src/app/farcaster/components/safe-area.tsx
+++ src/app/farcaster/components/safe-area.tsx
@@ -2,6 +2,12 @@
 
 import { useFarcaster } from "@/providers/farcaster-provider";
 
+function setProperty(name: string, value: string) {
+  if (typeof window !== "undefined") {
+    document.documentElement.style.setProperty(name, value);
+  }
+}
+
 export function SafeArea({
   children,
   topNavHeight = "0px",
@@ -13,36 +19,22 @@ export function SafeArea({
 }) {
   const { farcaster } = useFarcaster();
 
-  if (typeof window !== "undefined") {
-    document.documentElement.style.setProperty("--t-nav", topNavHeight);
-    document.documentElement.style.setProperty("--b-nav", bottomNavHeight);
-  }
+  setProperty("--t-nav", topNavHeight);
+  setProperty("--b-nav", bottomNavHeight);
 
   const safeAreaInsets = farcaster?.client?.safeAreaInsets;
 
   if (typeof safeAreaInsets !== "undefined") {
-    const verticalSafeAreaInsets = `${safeAreaInsets.top}px + ${safeAreaInsets.bottom}px`;
+    setProperty("--fc-safe-area-inset-top", `${safeAreaInsets.top}`);
+    setProperty("--fc-safe-area-inset-bottom", `${safeAreaInsets.bottom}`);
+    setProperty("--fc-safe-area-inset-left", `${safeAreaInsets.left}`);
+    setProperty("--fc-safe-area-inset-right", `${safeAreaInsets.right}`);
 
     return (
       <>
-        <div
-          className="bg-background pointer-events-none fixed top-0 right-0 left-0 z-99999"
-          style={{ height: `${safeAreaInsets.top}px` }}
-        />
-        <div
-          className="bg-background pointer-events-none fixed right-0 bottom-0 left-0 z-99999"
-          style={{ height: `${safeAreaInsets.bottom}px` }}
-        />
-        <div
-          className="px-safe"
-          style={{
-            marginRight: `${safeAreaInsets.right}px`,
-            marginLeft: `${safeAreaInsets.left}px`,
-            marginBottom: `calc(${safeAreaInsets.bottom}px + var(--b-nav))`,
-            marginTop: `calc(${safeAreaInsets.top}px + var(--t-nav))`,
-            minHeight: `calc(100dvh - (${verticalSafeAreaInsets} + var(--t-nav) + var(--b-nav)))`,
-          }}
-        >
+        <div className="bg-background pointer-events-none fixed top-0 right-0 left-0 z-99999 h-[env(--fc-safe-area-inset-top)]" />
+        <div className="bg-background pointer-events-none fixed right-0 bottom-0 left-0 z-99999 h-[env(--fc-safe-area-inset-bottom)]" />
+        <div className="px-safe mt-[calc(env(--fc-safe-area-inset-top)+var(--t-nav))] mr-[env(--fc-safe-area-inset-right)] mb-[calc(env(--fc-safe-area-inset-bottom)+var(--b-nav))] ml-[env(--fc-safe-area-inset-left)] min-h-[calc(100dvh-(env(--fc-safe-area-inset-top)+env(--fc-safe-area-inset-bottom)+var(--t-nav)+var(--b-nav)))]">
           <div className="max-w-global mx-auto">{children}</div>
         </div>
       </>
