3d5344b79cfb7d25e1e392d130ca8cf319ad9dec
Pablo <pahhhblo@proton.me>
2025-06-11 11:04:44 -0400

    fix farcaster safe area

--- src/app/farcaster/components/safe-area.tsx
+++ src/app/farcaster/components/safe-area.tsx
@@ -54,7 +54,7 @@ export function SafeArea({
     <>
       <div className="bg-background h-t-inset pointer-events-none fixed top-0 right-0 left-0 z-99999" />
       <div className="bg-background h-b-inset pointer-events-none fixed right-0 bottom-0 left-0 z-99999" />
-      <div className="px-safe mr-r-inset ml-l-inset mb-b-inset mt-t-inset min-h-content-inset">
+      <div className="px-safe mr-r-inset ml-l-inset mb-b-inset-nav mt-t-inset-nav min-h-content-inset">
         <div className="max-w-global mx-auto">{children}</div>
       </div>
     </>
