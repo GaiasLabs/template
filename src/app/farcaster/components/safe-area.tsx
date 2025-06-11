c1c9b0ff2a9ff6dfc6d5df839d387da78febe05f
Pablo <pahhhblo@proton.me>
2025-06-11 15:03:36 -0400

    replace env with var

--- src/app/farcaster/components/safe-area.tsx
+++ src/app/farcaster/components/safe-area.tsx
@@ -38,9 +38,9 @@ export function SafeArea({
 
     return (
       <>
-        <div className="bg-background pointer-events-none fixed top-0 right-0 left-0 z-99999 h-[env(--fc-safe-area-inset-top)]" />
-        <div className="bg-background pointer-events-none fixed right-0 bottom-0 left-0 z-99999 h-[env(--fc-safe-area-inset-bottom)]" />
-        <div className="px-safe mt-[calc(env(--fc-safe-area-inset-top)+var(--t-nav))] mr-[env(--fc-safe-area-inset-right)] mb-[calc(env(--fc-safe-area-inset-bottom)+var(--b-nav))] ml-[env(--fc-safe-area-inset-left)] min-h-[calc(100dvh-(env(--fc-safe-area-inset-top)+env(--fc-safe-area-inset-bottom)+var(--t-nav)+var(--b-nav)))]">
+        <div className="bg-background pointer-events-none fixed top-0 right-0 left-0 z-99999 h-[var(--fc-safe-area-inset-top)]" />
+        <div className="bg-background pointer-events-none fixed right-0 bottom-0 left-0 z-99999 h-[var(--fc-safe-area-inset-bottom)]" />
+        <div className="px-safe mt-[calc(var(--fc-safe-area-inset-top)+var(--t-nav))] mr-[var(--fc-safe-area-inset-right)] mb-[calc(var(--fc-safe-area-inset-bottom)+var(--b-nav))] ml-[var(--fc-safe-area-inset-left)] min-h-[calc(100dvh-(var(--fc-safe-area-inset-top)+var(--fc-safe-area-inset-bottom)+var(--t-nav)+var(--b-nav)))]">
           <div className="max-w-global mx-auto">{children}</div>
         </div>
       </>
